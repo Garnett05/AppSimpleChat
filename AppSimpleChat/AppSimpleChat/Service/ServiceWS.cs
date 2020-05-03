df6985f0e814dec5ba00551b46bcb689d4b446f0
Garnett05 <charlleslimamota10@hotmail.com>
2020-05-03 20:53:23 -0300

    ServiceWS - GetChats method

--- AppSimpleChat/AppSimpleChat/Service/ServiceWS.cs
+++ AppSimpleChat/AppSimpleChat/Service/ServiceWS.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Net;
 using System.Net.Http;
 using AppSimpleChat.Model;
+using Newtonsoft.Json;
 
 namespace AppSimpleChat.Service
 {
@@ -16,6 +17,7 @@ namespace AppSimpleChat.Service
             var url = urlBase + "/usuario";
 
             //StringContent param = new StringContent(string.Format("?nome={0}&password={1}", user.nome, user.password));
+            // Acima é uma maneira alternativa de fazermos oq é possível pela linha debaixo
             FormUrlEncodedContent param = new FormUrlEncodedContent(new[] {
                 new KeyValuePair<string, string>("nome", user.nome),
                 new KeyValuePair<string, string>("password", user.password)
@@ -24,9 +26,22 @@ namespace AppSimpleChat.Service
 
             HttpClient httpRequest = new HttpClient();
             HttpResponseMessage httpResponse = httpRequest.PostAsync(url, param).GetAwaiter().GetResult();
-            if (httpResponse == HttpStatusCode.OK)
+            if (httpResponse.StatusCode == HttpStatusCode.OK)
             {
                 //Deserializar, retornar no método e salvar como login
+            }
+            return null;
+        }
+        public static List<Chat> GetChats()
+        {
+            var url = urlBase + "/chats";
+
+            HttpClient request = new HttpClient();
+            HttpResponseMessage httpResponse = request.GetAsync(url).GetAwaiter().GetResult();
+            if (httpResponse.StatusCode == HttpStatusCode.OK)
+            {
+                string content = httpResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                List<Chat> list = JsonConvert.DeserializeObject<List<Chat>>(content);
             }
             return null;
         }
